5822e02b96e7a8367d6c5b1edd29725f9ea70a01
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-01-06 02:59:02 +0900

    Fix refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import {MuiThemeProvider, makeStyles} from '@material-ui/core/styles';
 import Home from './components/Home'
 import {NotFound} from './components/NotFound'
 
-const useStyles = makeStyles({
-  root: {
-      width: "100vw",
-      minHeight: "100vh",
-      overflow: "hidden",
-      backgroundColor: "rgba(245,245,245 ,1)",
-      position: "relative",
-      paddingBottom: "80px",
-      boxSizing: "border-box"
-  }
-});
-
 const App = () => {
-  // const classes = useStyles(props);
 
   return (
     <Switch>
