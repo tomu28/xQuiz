6a8486d52bd72bd75912d23d022978b5cedc6092
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-01-06 04:39:14 +0900

    add footer

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,49 @@
 import React from 'react';
+// Material-UI
+import { makeStyles } from '@material-ui/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ContactSupportOutlinedIcon from '@material-ui/icons/ContactSupportOutlined';
+import ChatIcon from '@material-ui/icons/Chat';
 
-function Chat(props){
+const useStyles = makeStyles({
+    root: {
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+    },
+});
 
-    return(
-        <>
-        チャット
-        </>
+function Chat(props) {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const changeRoutePath = (e, link) => {
+        e.preventDefault();
+        props.history.push(link);
+    };
+
+    return (        
+        <div>
+            <h2>Chat</h2>
+            <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+            setValue(newValue);
+            }}
+            showLabels
+            className={classes.root}
+        >
+        <BottomNavigationAction
+            label="Quiz"
+            icon={<ContactSupportOutlinedIcon />}
+            onClick={(e) => changeRoutePath(e, '/')}/>
+        <BottomNavigationAction
+            label="Chat"
+            icon={<ChatIcon />}
+            onClick={(e) => changeRoutePath(e, '/chat')}/>
+        </BottomNavigation>
+        </div>
     );
 }
 
