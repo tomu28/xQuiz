e4ddd9951f640731d919fbe0b434855b89d945e9
ShunMatsuoka <b1718652@planet.kanazawa-it.ac.jp>
2020-01-16 09:33:58 +0900

    クイズ機能にある回答が選択できないバグをcssを用いて修正

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/styles';
-import Button from '@material-ui/core/Button';
-
-import '../Quiz.css';
-
 function Answer(props){
     let classes = ['answer'];
 
@@ -13,15 +8,12 @@ function Answer(props){
     }
 
     return (
-        <Button
+        <button 
             value = {props.letter} 
             className = {classes.join(' ')} 
             onClick = {props.handleClick}>
-            <span className="letter">{props.letter}.</span>
-            <span className="btn-primary">
-                {props.answer}
-            </span>
-        </Button>
+            <span className="letter">{props.letter}.</span> {props.answer}
+        </button>
     );
 }
 
